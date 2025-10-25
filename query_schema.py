32a114512fa9f1367d59240bb547e73db7ef63af
Rafael <rafael@Caramelo.com>
2025-10-25 01:39:27 -0300

    basic directors manipulation

--- query_schema.py
+++ query_schema.py
@@ -4,3 +4,4 @@ from pydantic import BaseModel, Field, PositiveInt
 class Director(BaseModel):
     name: str = Field(..., example="Christopher Nolan")
     age: PositiveInt = Field(..., example=50)
+
--- server/main.py
+++ server/main.py
@@ -6,6 +6,7 @@ from query_schema import Director
 import db_core
 from fastapi import FastAPI
 from playhouse.shortcuts import model_to_dict
+from pydantic import PositiveInt
 
 app = FastAPI()
 db_core.initialize_db()
@@ -24,3 +25,52 @@ def create_director(director: Director) -> dict:
         "message": "Director created successfully.",
         "data": model_to_dict(new_director),
     }
+
+
+# Get one director by ID
+@app.get("/directors/{director_id}")
+async def get_director(director_id: int) -> dict:
+    try:
+        director = db_core.Directors.get_by_id(director_id)
+        return {
+            "message": "Director retrieved successfully.",
+            "data": model_to_dict(director),
+        }
+    except db_core.Directors.DoesNotExist:
+        return {"message": "Director not found."}
+
+
+# Get all directors
+@app.get("/directors/")
+async def get_all_directors(page: PositiveInt = 1, size: PositiveInt = 10) -> dict:
+    directors = db_core.Directors.select().paginate(page, size)
+    return {
+        "message": "Directors retrieved successfully.",
+        "data": [model_to_dict(director) for director in directors],
+    }
+
+# Delete a director by ID
+@app.delete("/directors/{director_id}")
+async def delete_director(director_id: int) -> dict:
+    try:
+        director = db_core.Directors.get_by_id(director_id)
+        director.delete_instance()
+        return {"message": "Director deleted successfully."}
+    except db_core.Directors.DoesNotExist:
+        return {"message": "Director not found."}
+
+
+# Update a director by ID
+@app.put("/directors/{director_id}")
+async def update_director(director_id: int, director: Director) -> dict:
+    try:
+        existing_director = db_core.Directors.get_by_id(director_id)
+        existing_director.name = director.name
+        existing_director.age = director.age
+        existing_director.save()
+        return {
+            "message": "Director updated successfully.",
+            "data": model_to_dict(existing_director),
+        }
+    except db_core.Directors.DoesNotExist:
+        return {"message": "Director not found."}
